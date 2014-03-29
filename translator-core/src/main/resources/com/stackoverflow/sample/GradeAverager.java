0700a178d3565a575f06487737b3d3d6c40acb52
MakotoTheKnight <makotothedragon@gmail.com>
2014-03-29 12:44:20 -0600

    Formatting

--- translator-core/src/main/resources/com/stackoverflow/sample/GradeAverager.java
+++ translator-core/src/main/resources/com/stackoverflow/sample/GradeAverager.java
@@ -21,62 +21,54 @@ public class GradeAverager {
         System.out.println();
         System.out.println("The average is: " + average);
 
-        try{
-
-
-
-            if(score1 < score2 && score1 < score3) {
+        try {
+            if (score1 < score2 && score1 < score3) {
                 System.out.println("The lowest score was:" + score1);
-            } else if(score2 < score1 && score2 < score3) {
+            } else if (score2 < score1 && score2 < score3) {
                 System.out.println("The lowest score was:" + score2);
-            } else if(score3 < score1 && score3 < score2) {
+            } else if (score3 < score1 && score3 < score2) {
                 System.out.println("The lowest score was:" + score3);
             }
 
-            if(score1 < score2 && score1 < score3) {
+            if (score1 < score2 && score1 < score3) {
                 average_no_lowest = (score2 + score3) / 2.0;
                 System.out.println("The average without the lowest score is: " + average_no_lowest);
-                if(average_no_lowest > 90) {
+                if (average_no_lowest > 90) {
                     System.out.print('A');
-                } else if(average_no_lowest < 90 && average_no_lowest > 80) {
+                } else if (average_no_lowest < 90 && average_no_lowest > 80) {
                     System.out.print('B');
-                } else if(average_no_lowest > 70 && average_no_lowest < 80) {
+                } else if (average_no_lowest > 70 && average_no_lowest < 80) {
                     System.out.print('C');
                 } else {
                     System.out.print('D');
                 }
-            } else if(score2 < score1 && score2 < score3) {
+            } else if (score2 < score1 && score2 < score3) {
                 average_no_lowest = (score1 + score3) / 2.0;
                 System.out.println("The average without the lowest score is: " + average_no_lowest);
-                if(average_no_lowest > 90) {
+                if (average_no_lowest > 90) {
                     System.out.print('A');
-                } else if(average_no_lowest < 90 && average_no_lowest > 80) {
+                } else if (average_no_lowest < 90 && average_no_lowest > 80) {
                     System.out.print('B');
-                } else if(average_no_lowest > 70 && average_no_lowest < 80) {
+                } else if (average_no_lowest > 70 && average_no_lowest < 80) {
                     System.out.print('C');
                 } else {
                     System.out.print('D');
                 }
-            } else if(score3 < score1 && score3 < score2) {
+            } else if (score3 < score1 && score3 < score2) {
                 average_no_lowest = (score1 + score3) / 2.0;
                 System.out.println("The average without the lowest score is: " + (score1 + score3) / 2.0);
-                if(average_no_lowest > 90) {
+                if (average_no_lowest > 90) {
                     System.out.print('A');
-                } else if(average_no_lowest < 90 && average_no_lowest > 80) {
+                } else if (average_no_lowest < 90 && average_no_lowest > 80) {
                     System.out.print('B');
-                } else if(average_no_lowest > 70 && average_no_lowest < 80) {
+                } else if (average_no_lowest > 70 && average_no_lowest < 80) {
                     System.out.print('C');
                 } else {
                     System.out.print('D');
                 }
             }
-
-        }   catch (Exception e){
-
+        } catch (Exception e) {
             System.out.println(e);
-
         }
-
-
     }
 }
